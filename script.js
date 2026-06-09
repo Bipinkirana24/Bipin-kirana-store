6d760fa5a8705161f6c96e77fcdd77f92079432f
Bipinkirana24 <pennypeak7@gmail.com>
2026-06-10 01:32:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let cart = [];
 
-function addToCart(name, price) {
+// Add Product
+function addToCart(name, price){
 
 const item = cart.find(p => p.name === name);
 
@@ -18,6 +19,7 @@ qty: 1
 updateCart();
 }
 
+// Remove Product
 function removeFromCart(name){
 
 const item = cart.find(p => p.name === name);
@@ -33,23 +35,33 @@ cart = cart.filter(p => p.name !== name);
 updateCart();
 }
 
+// Update Cart
 function updateCart(){
 
 let total = 0;
 let count = 0;
 
 cart.forEach(item => {
+
 total += item.price * item.qty;
 count += item.qty;
+
 });
 
 const cartCount = document.getElementById("cartCount");
 const cartTotal = document.getElementById("cartTotal");
 
-if(cartCount) cartCount.innerText = count;
-if(cartTotal) cartTotal.innerText = total;
+if(cartCount){
+cartCount.innerText = count;
 }
 
+if(cartTotal){
+cartTotal.innerText = total;
+}
+
+}
+
+// Show Cart
 function showCart(){
 
 if(cart.length === 0){
@@ -60,20 +72,41 @@ return;
 let text = "Cart Items:\n\n";
 
 cart.forEach(item => {
-text += item.name + " x " + item.qty + "\n";
+
+text += item.name +
+" x " +
+item.qty +
+"\n";
+
 });
 
 alert(text);
+
 }
 
+// Place Order
 function placeOrder(){
 
-const name = document.getElementById("custName").value;
-const mobile = document.getElementById("custMobile").value;
-const address = document.getElementById("custAddress").value;
+const name =
+document.getElementById("custName").value;
 
-if(name === "" || mobile === "" || address === ""){
+const mobile =
+document.getElementById("custMobile").value;
+
+const address =
+document.getElementById("custAddress").value;
+
+if(
+name === "" ||
+mobile === "" ||
+address === ""
+){
 alert("Please fill all details");
+return;
+}
+
+if(cart.length === 0){
+alert("Cart Empty");
 return;
 }
 
@@ -82,29 +115,51 @@ let total = 0;
 
 cart.forEach(item => {
 
-orderItems += item.name + " x " + item.qty + "\n";
+orderItems +=
+item.name +
+" x " +
+item.qty +
+"\n";
 
-total += item.price * item.qty;
+total +=
+item.price *
+item.qty;
 
 });
 
 let message =
+
 "New Order - Bipin Kirana Store\n\n" +
-"Customer Name: " + name + "\n" +
-"Mobile: " + mobile + "\n" +
-"Address: " + address + "\n\n" +
+
+"Customer Name: " +
+name +
+"\n\n" +
+
+"Mobile: " +
+mobile +
+"\n\n" +
+
+"Address: " +
+address +
+"\n\n" +
+
 "Ordered Items:\n" +
-orderItems + "\n" +
-"Total Amount: ₹" + total + "\n\n" +
+orderItems +
+"\n" +
+
+"Total Amount: ₹" +
+total +
+"\n\n" +
+
 "Payment: Cash on Delivery";
-alert(orderItems);
-alert(total);
+
 alert(message);
 
 /*
-WhatsApp Number Later
 
-const whatsappNumber = "YOUR_NUMBER_HERE";
+WhatsApp Later
+
+const whatsappNumber = "";
 
 window.open(
 "https://wa.me/" +
@@ -112,6 +167,7 @@ whatsappNumber +
 "?text=" +
 encodeURIComponent(message)
 );
+
 */
 
 }
